6eab5ff7688eece23b1c192f058d0accf114edd7
Jeff Muizelaar <jrmuizel@gmail.com>
2023-03-15 16:38:26 -0400

    Polyfill the missing JSC things sooner

--- resources/todomvc/architecture-examples/backbone/shell-polyfill-hack.js
+++ resources/todomvc/architecture-examples/backbone/shell-polyfill-hack.js
@@ -8,6 +8,15 @@ DumpMissingPropertiesBase.prototype = new Proxy({}, {
 });
 
 var timeoutHandlers = [];
+if (!globalThis.console) {
+    globalThis.console = { log: (msg) => print(msg) }
+}
+globalThis.console.error = globalThis.console.log;
+globalThis.console.warn = globalThis.console.log;
+// For JSC
+if (!globalThis.performance) {
+    globalThis.performance = { now: () => preciseTime()*1000 }
+}
 
 // In jsshell, mock just enough details of DOM for matrix to work.
 if (!("window" in globalThis)) {
@@ -534,14 +543,5 @@ if (!("window" in globalThis)) {
 
     globalThis.document.appendChild(globalThis.document.documentElement);
     globalThis.document.documentElement.appendChild(globalThis.document.body);
-    if (!globalThis.console) {
-	globalThis.console = { log: (msg) => print(msg) }
-    }
-    globalThis.console.error = globalThis.console.log;
-    globalThis.console.warn = globalThis.console.log;
-    // For JSC
-    if (!globalThis.performance) {
-	globalThis.performance = { now: () => preciseTime()*1000 }
-    }
     globalThis.onhashchange = null
 } // !("window" in globalThis)
