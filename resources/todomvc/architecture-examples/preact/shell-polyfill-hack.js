11e01be28434d7dd558332b054d05a875ce16a1e
Jeff Muizelaar <jrmuizel@gmail.com>
2023-04-10 14:14:51 -0400

    Add support for running in JSC

--- resources/todomvc/architecture-examples/preact/shell-polyfill-hack.js
+++ resources/todomvc/architecture-examples/preact/shell-polyfill-hack.js
@@ -17,6 +17,15 @@ if (!("assert" in console)) {
         }
     }
 }
+if (!globalThis.console) {
+    globalThis.console = { log: (msg) => print(msg) }
+}
+globalThis.console.error = globalThis.console.log;
+globalThis.console.warn = globalThis.console.log;
+// For JSC
+if (!globalThis.performance) {
+    globalThis.performance = { now: () => preciseTime() * 1000 }
+}
 
 // In jsshell, mock just enough details of DOM for matrix to work.
 if (!("window" in globalThis)) {
@@ -689,14 +698,5 @@ if (!("window" in globalThis)) {
 
     globalThis.document.appendChild(globalThis.document.documentElement);
     globalThis.document.documentElement.appendChild(globalThis.document.body);
-    if (!globalThis.console) {
-	globalThis.console = { log: (msg) => print(msg) }
-    }
-    globalThis.console.error = globalThis.console.log;
-    globalThis.console.warn = globalThis.console.log;
-    // For JSC
-    if (!globalThis.performance) {
-	globalThis.performance = { now: () => preciseTime()*1000 }
-    }
     globalThis.onhashchange = null
 } // !("window" in globalThis)
--- resources/todomvc/architecture-examples/react/shell-polyfill-hack.js
+++ resources/todomvc/architecture-examples/react/shell-polyfill-hack.js
@@ -17,6 +17,16 @@ if (!("assert" in console)) {
         }
     }
 }
+if (!globalThis.console) {
+    globalThis.console = { log: (msg) => print(msg) }
+}
+globalThis.console.error = globalThis.console.log;
+globalThis.console.warn = globalThis.console.log;
+// For JSC
+if (!globalThis.performance) {
+    globalThis.performance = { now: () => preciseTime() * 1000 }
+}
+
 
 // In jsshell, mock just enough details of DOM for matrix to work.
 if (!("window" in globalThis)) {
@@ -683,14 +693,5 @@ if (!("window" in globalThis)) {
 
     globalThis.document.appendChild(globalThis.document.documentElement);
     globalThis.document.documentElement.appendChild(globalThis.document.body);
-    if (!globalThis.console) {
-	globalThis.console = { log: (msg) => print(msg) }
-    }
-    globalThis.console.error = globalThis.console.log;
-    globalThis.console.warn = globalThis.console.log;
-    // For JSC
-    if (!globalThis.performance) {
-	globalThis.performance = { now: () => preciseTime()*1000 }
-    }
     globalThis.onhashchange = null
 } // !("window" in globalThis)
