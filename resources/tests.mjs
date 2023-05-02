d1d0edb7a23049b61306d1b9b3113a8edd6a9b63
Brian Grinstead <briangrinstead@gmail.com>
2023-05-02 14:25:04 -0700

    update the resizing test

--- resources/tests.mjs
+++ resources/tests.mjs
@@ -370,13 +370,17 @@ Suites.push({
         await page.waitForElement("html.ready");
     },
     tests: [
-        new BenchmarkTestStep("Medium", (page) => {
-            page.querySelector("iframe").setAttribute("class", "medium");
-        }),
-        new BenchmarkTestStep("Small", (page) => {
+        new BenchmarkTestStep("ResizeToSmall", (page) => {
             page.querySelector("iframe").setAttribute("class", "small");
         }),
-        new BenchmarkTestStep("Big", (page) => {
+        new BenchmarkTestStep("OpenNavPopup", (page) => {
+            page.querySelector("#OpenNavPopup").click();
+        }),
+        new BenchmarkTestStep("ExpandNavPopup", (page) => {
+            page.querySelector("#ExpandNavPopup").click();
+        }),
+        new BenchmarkTestStep("ResizeToBig", (page) => {
+            page.querySelector("#CloseNavPopup").click();
             page.querySelector("iframe").setAttribute("class", "");
         }),
     ],
