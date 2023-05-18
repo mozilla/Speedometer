a6891a3ae15a13c36ba1cab0a2d0caa454528217
Brian Grinstead <briangrinstead@gmail.com>
2023-05-18 12:40:46 -0700

    switch to shift

--- resources/developer-mode.mjs
+++ resources/developer-mode.mjs
@@ -70,7 +70,7 @@ export function createUIForSuites(suites) {
         checkbox.checked = !suite.disabled;
         checkbox.onclick = (e) => {
             suite.disabled = !checkbox.checked;
-            if (e?.ctrlKey) {
+            if (e?.shiftKey) {
                 for (var suiteIndex = 0; suiteIndex < suites.length; suiteIndex++) {
                     if (suites[suiteIndex] !== suite) {
                         suites[suiteIndex].disabled = true;
