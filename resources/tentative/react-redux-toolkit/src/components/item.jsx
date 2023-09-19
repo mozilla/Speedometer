7a8802b19f6eb6e493ade9d4259cb61cbc28c35b
Julien Wajsberg <felash@gmail.com>
2023-09-19 15:16:36 +0200

    Moved to functional components

--- resources/tentative/react-redux-toolkit/src/components/item.jsx
+++ resources/tentative/react-redux-toolkit/src/components/item.jsx
@@ -1,63 +1,55 @@
-import { PureComponent } from "react";
+import { useState, memo } from "react";
 import classnames from "classnames";
 import PropTypes from "prop-types";
 import TextInput from "./text-input";
 
-export default class Item extends PureComponent {
-    static propTypes = {
-        todo: PropTypes.object.isRequired,
-        editTodo: PropTypes.func.isRequired,
-        deleteTodo: PropTypes.func.isRequired,
-        toggleTodo: PropTypes.func.isRequired,
-        index: PropTypes.number.isRequired,
-    };
+function Item({ todo, toggleTodo, deleteTodo, index, editTodo }) {
+    const [isEditing, setIsEditing] = useState(false);
 
-    state = {
-        editing: false,
-    };
-
-    handleDoubleClick = () => {
-        this.setState({ editing: true });
-    };
-
-    handleSave = (id, text) => {
+    const handleSave = (text) => {
         if (text.length === 0)
-            this.props.deleteTodo(id);
+            deleteTodo(todo.id);
         else
-            this.props.editTodo({ id, text });
+            editTodo({ id: todo.id, text });
 
-        this.setState({ editing: false });
+        setIsEditing(false);
     };
 
-    render() {
-        const { todo, toggleTodo, deleteTodo, index } = this.props;
-
-        let element;
-        if (this.state.editing) {
-            element = <TextInput text={todo.text} editing={this.state.editing} onSave={(text) => this.handleSave(todo.id, text)} />;
-        } else {
-            element = (
-                <div className="view">
-                    <input className="toggle" type="checkbox" data-testid="todo-item-toggle" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
-                    <label onDoubleClick={this.handleDoubleClick} data-testid="todo-item-label">
-                        {todo.text}
-                    </label>
-                    <button className="destroy" data-testid="todo-item-button" onClick={() => deleteTodo(todo.id)} />
-                </div>
-            );
-        }
-
-        return (
-            <li
-                className={classnames({
-                    completed: todo.completed,
-                    editing: this.state.editing,
-                })}
-                data-testid="todo-item"
-                data-priority={4 - (index % 5)}
-            >
-                {element}
-            </li>
+    let element;
+    if (isEditing) {
+        element = <TextInput text={todo.text} editing={isEditing} onSave={handleSave} />;
+    } else {
+        element = (
+            <div className="view">
+                <input className="toggle" type="checkbox" data-testid="todo-item-toggle" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
+                <label onDoubleClick={() => setIsEditing(true)} data-testid="todo-item-label">
+                    {todo.text}
+                </label>
+                <button className="destroy" data-testid="todo-item-button" onClick={() => deleteTodo(todo.id)} />
+            </div>
         );
     }
+
+    return (
+        <li
+            className={classnames({
+                completed: todo.completed,
+                editing: isEditing,
+            })}
+            data-testid="todo-item"
+            data-priority={4 - (index % 5)}
+        >
+            {element}
+        </li>
+    );
 }
+
+export default memo(Item);
+
+Item.propTypes = {
+    todo: PropTypes.object.isRequired,
+    editTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired,
+    toggleTodo: PropTypes.func.isRequired,
+    index: PropTypes.number.isRequired,
+};
